7de94bd41cb9fbe54a2d235a3ee1e79aa95a6cbd
atulmittalg <55511902+atulmittalg@users.noreply.github.com>
2020-02-12 14:40:39 +0100

    Adding new test case checking if 15 translates to FizzBuzz

--- src/main/java/com/kata/FizzBuzz.java
+++ src/main/java/com/kata/FizzBuzz.java
@@ -2,12 +2,16 @@ package main.java.com.kata;
 
 public class FizzBuzz {
     public Object translate(int numberToTranslate) {
+        if(numberToTranslate % 3 == 0 && numberToTranslate % 5 == 0){
+            return "FizzBuzz";
+        }
         if(numberToTranslate % 3 == 0){
             return "Fizz";
         }
         if(numberToTranslate % 5 == 0){
             return "Buzz";
         }
+
         return numberToTranslate;
     }
 }
