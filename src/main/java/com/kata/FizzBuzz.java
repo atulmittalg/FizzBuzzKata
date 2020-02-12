7aa611ee6b7def6ce2cb7cea6f5ef5861f260d07
atulmittalg <55511902+atulmittalg@users.noreply.github.com>
2020-02-12 14:51:41 +0100

    Refactoring the translate method, extracting isFizz and isBuzz from translate

--- src/main/java/com/kata/FizzBuzz.java
+++ src/main/java/com/kata/FizzBuzz.java
@@ -2,16 +2,23 @@ package main.java.com.kata;
 
 public class FizzBuzz {
     public Object translate(int numberToTranslate) {
-        if(numberToTranslate % 3 == 0 && numberToTranslate % 5 == 0){
-            return "FizzBuzz";
-        }
-        if(numberToTranslate % 3 == 0){
-            return "Fizz";
-        }
-        if(numberToTranslate % 5 == 0){
-            return "Buzz";
-        }
+        if(isFizz(numberToTranslate) && isBuzz(numberToTranslate)) return "FizzBuzz";
+        else if (isFizz(numberToTranslate)) return "Fizz";
+        else if (isBuzz(numberToTranslate)) return "Buzz";
+        else return numberToTranslate;
+    }
 
-        return numberToTranslate;
+    private boolean isBuzz(int numberToTranslate) {
+        if (numberToTranslate % 5 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFizz(int numberToTranslate) {
+        if (numberToTranslate % 3 == 0) {
+            return true;
+        }
+        return false;
     }
 }
